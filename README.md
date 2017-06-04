b3e5c280c26763e5dc72b5426109363a36c0531d
Niko Schmuck <niko@nava.de>
2017-06-04 15:49:35 +0200

    incarnation.

---
+++ README.md
@@ -0,0 +1,4 @@
+## dashboard-monitor
+
+Display the most relevant parts of your application infrastructure on one dashboard page.
+UI part is inspired on dashing. 
---
+++ src/main/java/de/nava/DashboardMonitorApplication.java
@@ -0,0 +1,12 @@
+package de.nava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardMonitorApplication.class, args);
+	}
+}
